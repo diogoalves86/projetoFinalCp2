808b2e553af9da725d3aa8f0fa4947bf3378d2f0
Diogo <diogoalvessilva86@gmail.com>
2015-07-22 19:20:05 -0300

    Implementando sistema de Login e cadastro. Erro no banco de dados

---
+++ ProjetoFinalCp2/Logado.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Logado : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+    {
+        Context.GetOwinContext().Authentication.SignOut();
+    }
+}
